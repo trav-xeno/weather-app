f13dacca7890bae2df693a72b7d013db0f8cdad9
Travis <travisnevins@outlook.com>
2020-02-09 22:18:13 -0500

    remove js file

--- assests/js/annoyed.js
+++
@@ -1,7 +0,0 @@
-axios({
-  method: "get",
-  url: "http://bit.ly/2mTM3nY",
-  responseType: "stream"
-}).then(function(response) {
-  response.data.pipe(fs.createWriteStream("ada_lovelace.jpg"));
-});
